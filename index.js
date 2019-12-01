a2076a35a4e58a7b377f2fa01ab1b560b151338b
goldenjayr <goldenjayr@gmail.com>
2019-12-02 07:12:17 +0800

    modified server

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { addUser, removeUser, getUser, getUsersInRoom } = require('./users')
 
 
 io.on('connection', (socket) => {
+    console.log('new Connection')
     socket.on('user-join', ({ name, room}, callback) => {
         const { error, user } = addUser({id: socket.id, name, room})
         if (error) return callback(error)
@@ -14,18 +15,27 @@ io.on('connection', (socket) => {
         socket.emit('admin-message', {user: 'admin', text: `${user.name}, Welcome to the room ${user.room}.`})
         socket.broadcast.to(user.room).emit('admin-message', {user: 'admin', text: `${user.name} has joined.`})
         socket.join(user.room)
+        io.to(user.room).emit('room-data', {room: user.room, users: getUsersInRoom(user.room)})
+
     })
 
     socket.on('send-message', (message, callback) => {
         const user = getUser(socket.id)
 
         io.to(user.room).emit('admin-message', {user: user.name, text: message})
+        io.to(user.room).emit('room-data', {room: user.room, users: getUsersInRoom(user.room)})
+
         callback()
     })
 
 
     socket.on('disconnect', () => {
-        console.log('User has disconnected')
+        console.log('Disconnection')
+        // const user = removeUser(socket.id)
+
+        // if (user) {
+            // io.to(user.room).emit('admin-message', {user: 'admin', text: `${user.name} has left.`})
+        // }
     })
 } )
 
--- users.js
+++ users.js
@@ -12,6 +12,7 @@ const addUser = ({id, name, room}) => {
 
     const user = { id, name, room}
     users.push(user)
+    console.log(users)
 
     return { user }
 }
@@ -19,7 +20,11 @@ const addUser = ({id, name, room}) => {
 const removeUser = id => {
     const user = users.find(user => user.id === id)
     const remainingUsers = users.filter(user => user.id !== id)
+    console.log("TCL: remainingUsers", remainingUsers)
+    console.log(users)
     users = [...remainingUsers]
+    console.log("TCL: users", users)
+
     return user
 }
 
